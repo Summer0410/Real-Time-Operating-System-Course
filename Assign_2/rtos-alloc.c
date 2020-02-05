7ac4b05242fbf8ec53596951b76d5fab3a0f53b4
Linqiao Xia <lx2786@slbnen3000pc36.engr.mun.ca>
2020-02-05 13:31:41 -0330

    Add A2

---
+++ Assign_2/rtos-alloc.c
@@ -0,0 +1,97 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h> 
+#include <fcntl.h>
+#include <rtos-alloc.h>
+/*
+ * This macro expands to `extern "C" {` when compiling with C++ (e.g., when
+ * compiling test code that includes this header file) and expands to nothing
+ * otherwise (e.g., when compiling C code).
+ */
+__BEGIN_DECLS
+
+
+/*
+ * The following functions are declartions of standard C library functions
+ * for you to implement:
+ */
+
+/**
+ * Allocate @b size bytes of memory for the exclusive use of the caller,
+ * as `malloc(3)` would.
+ */
+void*	rtos_malloc(size_t size){
+        void *addr = mmap(NULL, size, PROT_READ,MAP_SHARED,-1,0);
+        return addr;
+}
+
+/**
+ * Change the size of the allocation starting at @b ptr to be @b size bytes,
+ * as `realloc(3)` would.
+ */
+void*	rtos_realloc(void *ptr, size_t size){
+        void *addr = mmap(NULL, size, PROT_READ,MAP_SHARED,0,0);
+        return addr;
+}
+
+/**
+ * Release the memory allocation starting at @b ptr for general use,
+ * as `free(3)` would.
+ */
+void	rtos_free(void *ptr){
+        void *addr = mmap(NULL, ptr, PROT_READ,MAP_SHARED,0,0);
+}
+
+
+/*
+ * The following functions will help our test code inspect your allocator's
+ * internal state:
+ */
+
+/**
+ * How large is the allocation pointed at by this pointer?
+ *
+ * @pre   @b ptr points at a valid allocation (according to `rtos_allocated`)
+ */
+size_t	rtos_alloc_size(void *ptr){
+        return 0;
+}
+
+/**
+ * Does this pointer point at the beginning of a valid allocation?
+ *
+ * @param   ptr    any virtual address
+ *
+ * @returns whether or not @b ptr is a currently-allocated address returned
+ *          from @b my_{m,re}alloc
+ */
+bool	rtos_allocated(void *ptr){
+        return false;
+}
+
+/**
+ * How much memory has been allocated by this allocator?
+ *
+ * @returns the number of bytes that have been allocated to user code,
+ *          **not** including any allocator overhead
+ */
+size_t	rtos_total_allocated(void){
+        return 0;
+}
+
+bool rtos_is_valid(void *ptr){
+        return true;
+}
+/*
+ * This macro expands to `}` to close the `extern "C"` block when compiling C++
+ * and expands to nothing otherwise.
+ */
+__END_DECLS
+
+
+
+
+
+
